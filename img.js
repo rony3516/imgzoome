74b54dc8013ec0424b693902435fdbdbea7fdf21
rony3516 <121095564+rony3516@users.noreply.github.com>
2023-05-23 09:48:18 +0600

    Add files via upload

---
+++ img.js
@@ -0,0 +1,10 @@
+let mainimg=document.getElementById('mainimg');
+function change(){
+    mainimg.src="img/download.jfif";
+}
+function change1(){
+    mainimg.src="img/download (2).jfif";
+}
+function change2(){
+    mainimg.src="img/download (3).jfif";
+}
